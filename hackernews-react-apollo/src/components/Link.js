9088057c4957a8940c16a64de8385243a857ad33
Aaron Pleitez <pleitez.aaron12@gmail.com>
2020-06-17 11:50:20 -0500

    Creating A Link Creating part of Crud, on gql

--- hackernews-react-apollo/src/components/Link.js
+++ hackernews-react-apollo/src/components/Link.js
@@ -4,7 +4,6 @@ class Link extends Component {
   render() {
     return (
       <div>
-        <h1>hey</h1>
         <div>
           {this.props.link.description}({this.props.link.url})
         </div>
